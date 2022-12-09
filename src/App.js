53d020b3d1d636eab185319e3b92e546aed19dae
hadaroadams <hadaroadams1234@gmail.com>
2022-12-09 07:26:03 +0000

    product building

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Header from './Header.jsx';
+import Home from './Home.jsx'
+import Product from './Product';
 function App() {
   return (
     <div className="App">
       <Header/>
-      {/*Home*/}
+      <Home/>
+      <Product/>
     </div>
   );
 }
